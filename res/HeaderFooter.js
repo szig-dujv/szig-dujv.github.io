88350c73b8dd9e3b20816c83ae99f64e12718fce
szig-dujv <harcsa.pinter.barnabas@gmail.com>
2026-07-03 11:12:52 +0200

    Update HeaderFooter.js

--- res/HeaderFooter.js
+++ res/HeaderFooter.js
@@ -65,7 +65,9 @@ class MyHeader extends HTMLElement {
 					<a href="/" class="nav-b">Főoldal</a>
 				</li>
                 <li>
-					<a href="https://szig-dujv.hu/" class="nav-b">szig-dujv.hu/</a>
+					<a href="https://szig-dujv.hu/" class="nav-b">
+					<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" height="16" width="16" fill="none"><g id="LMS-logo"> <g id="Interface / External_Link"> <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke-width="2" stroke-linejoin="round" stroke-linecap="round"></path> </g> </g></svg>
+					szig-dujv.hu</a>
 				</li>
 				<a class="G-class" href="https://classroom.google.com/" rel="noopener noreferrer" target="_blank">
 					<span class="LMS-logo">
